f7ee22740f54aef9fa810c174a8826370bc76f61
Rafanomezantsoa1 <lalainarafanomezantsoaa@gmail.com>
2026-02-04 13:35:29 +0100

    test de Lalaina

---
+++ Script1.sql
@@ -0,0 +1,11 @@
+Create database projetmrnaina;
+
+create table type(
+    id_type int serial primary key,
+    nom varchar(255) not null
+);
+create table vehicule(
+    id_vehicule int serial primary key,
+    id_type int references type(id_type),
+    capacite int not null
+);
